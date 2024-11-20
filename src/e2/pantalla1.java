bb19e746a6896a6a9124e009b19007501cd4ed4f
dhsib-XD <carlosgeorodriguez11@gmail.com>
2024-11-20 11:04:10 -0600

    Aqui le dejo mi traba Ing.

---
+++ src/e2/pantalla1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package e2;
+
+/**
+ *
+ * @author CarlosXl
+ */
+class pantalla1 {
+    
+}
